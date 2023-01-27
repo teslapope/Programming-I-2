7290b5322ac5ba619ebd3fe33cd3110cc9b8cb70
teslapope <123481177+teslapope@users.noreply.github.com>
2023-01-27 15:09:09 +0000

    update

---
+++ k26.py
@@ -0,0 +1,41 @@
+from stanfordkarel import *
+
+
+def main():
+  """Karel code goes here!"""
+  move()
+  move()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  pick_beeper()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  
+  pass
+
+if __name__ == "__main__":
+    run_karel_program()
