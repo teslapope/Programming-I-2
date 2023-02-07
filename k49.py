f21a8b893672a8541bae3fa36973978c9b430cea
teslapope <123481177+teslapope@users.noreply.github.com>
2023-02-07 15:11:18 +0000

    update

--- k49.py
+++ k49.py
@@ -3,131 +3,126 @@ from time import sleep
 
 
 class ktools:
-def m(self):
-  """Shorthand for move"""
-  move()
-  
-def tl(self):
-  """turn left"""
-  turn_left()
-  
-def tr(self):
-  """turn right"""
-  self.tl()
-  self.tl()
-  self.tl()
-  
-def ta(self):
-  """Turn around"""
-  self.tl()
-  self.tl()
-
-def pick(self):
-  """pick beeper"""
-  pick_beeper()
-
-def put(self):
-  """put beeper"""
-  put_beeper()
-
-def put2(self):
-  """put two beepers in a line"""
-  self.put()
-  self.m()
-  self.put()
-
-def put5(self):
-  """put five beepers in a line"""
-  self.put2()
-  self.m()
-  self.put2()
-  self.m()
-  self.put()
-
-def h(self):
-  """print H using beepers"""
-  self.tl()
-  self.put5()
-  self.tr()
-  self.m()
-  self.m()
-  self.m()
-  self.tr()
-  self.put5()
-  self.ta()
-  self.m()
-  self.m()
-  self.tl()
-  self.put2()
-  self.tl()
-  self.m()
-  self.m()
-  self.tl()
-  self.m()
-  self.m()
-  self.m()
-  self.m()
-
-def e(self):
-  """Print E using beepers"""
-
-def l(self):
-  """Print L using beepers"""
-
-def o(self):
-  """Print O using beepers"""
-
-  
-def main():
-    """Karel code goes here!"""
-    kt = ktools()
-    pass
-
-def fic(self):
-  """Front is clear"""
-  return front_is_clear()
-
-def fib(self):
-  """Front is blocked"""
-  return front_is_blocked()
-
-def ric(self):
-  """Right is clear"""
-  self.tr()
-  if self.fic(): 
+  def m(self):
+    """Shorthand for move"""
+    move()
+    
+  def tl(self):
+    """turn left"""
+    turn_left()
+    
+  def tr(self):
+    """turn right"""
     self.tl()
-    return True #immediately exit the function
-  self.tl()
-  return False 
-
-def rib(self):
-  """Right is blocked"""
-  return not self.ric()
-
-def mazemove(self):
-  """Maze move"""
-  if self.fib()
     self.tl()
-else:  #Otherwise...
-  self.m()
-  if self.ric()
+    self.tl()
+    
+  def ta(self):
+    """Turn around"""
+    self.tl()
+    self.tl()
+  
+  def pick(self):
+    """pick beeper"""
+    pick_beeper()
+  
+  def put(self):
+    """put beeper"""
+    put_beeper()
+  
+  def put2(self):
+    """put two beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+  
+  def put5(self):
+    """put five beepers in a line"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+  
+  def h(self):
+    """print H using beepers"""
+    self.tl()
+    self.put5()
     self.tr()
     self.m()
-    if self.ric()
-      self.tr()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+  
+  def e(self):
+    """Print E using beepers"""
+    pass
+  def l(self):
+    """Print L using beepers"""
+    pass
+  def o(self):
+    """Print O using beepers"""
+    pass
+  
+  def fic(self):
+    """Front is clear"""
+    return front_is_clear()
+  
+  def fib(self):
+    """Front is blocked"""
+    return front_is_blocked()
+  
+  def ric(self):
+    """Right is clear"""
+    self.tr()
+    if self.fic(): 
+      self.tl()
+      return True #immediately exit the function
+    self.tl()
+    return False 
+  
+  def rib(self):
+    """Right is blocked"""
+    return not self.ric()
+  
+  def mazemove(self):
+    """Maze move"""
+    if self.fib():
+      self.tl()
+    else:  #Otherwise...
       self.m()
-pass
-
+      if self.ric():
+        self.tr()
+        self.m()
+        if self.ric():
+          self.tr()
+          self.m()
+    pass
+  
 def main():
   """Karel code goes here!"""
   kt = ktools()
   kt.m()
   kt.tl()
   kt.m()
-  kt.mavemove()
+  kt.mazemove()
   sleep(3)
 
-
+  kt.m()
 
 if __name__ == "__main__":
     run_karel_program()
